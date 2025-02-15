a99e64aff5c08b2b38d9e009121e6e8187f4ce5f
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-15 19:10:29 +0300

    add Calculator

---
+++ src/main/java/calculator/java/Calculator.java
@@ -0,0 +1,35 @@
+package calculator.java;
+
+public class Calculator{
+
+    public Calculator() {
+    }
+
+    public Double sum(Double a, Double b){
+        return a + b;
+    }
+
+    public Integer sum(Integer a, Integer b){
+        return a + b;
+    }
+
+    public Double subtract(Double a, Double b){
+        return a - b;
+    }
+
+    public Integer subtract(Integer a, Integer b){
+        return a - b;
+    }
+
+    public Double multiply(Double a, Double b){
+        return a * b;
+    }
+
+    public Integer multiply(Integer a, Integer b){
+        return a * b;
+    }
+
+    public Double divide(Double a, Double b){
+        return a / b;
+    }
+}
