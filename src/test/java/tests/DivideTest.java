a3d40b1d8434c6e0141ff33dcfd19b01244adb72
Sergeant Svyatskiy <sithserega@gmail.com>
2025-02-17 18:47:44 +0300

    fix log

--- src/test/java/tests/DivideTest.java
+++ src/test/java/tests/DivideTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertTrue;
 
-public class DivideTest extends BaseTest{
+public class DivideTest extends BaseTest {
 
     @Test(priority = 1, description = "checking the division of positive numbers")
     public void divisionOfPositiveNumbersTest(){
@@ -20,14 +20,13 @@ public class DivideTest extends BaseTest{
         };
     }
     @Test(dataProvider = "divide", priority = 3, description = "checking the division of negative numbers")
-    public void divisionOfNegativeNumbersTest(Double a, Double b, Double result){
+    public void divisionOfNegativeNumbersTest(Double a, Double b, Double result) {
         Assert.assertEquals(calculator.divide(a, b), result);
     }
 
     @Test(retryAnalyzer = Retry.class, priority = 3, description = "checking the division of numbers by zero")
     public void divideByZeroTest() {
-        assertTrue(Double.isInfinite(calculator.divide(3.0, 0.0)));
-        System.out.println("Divide by zero gives infinity as result");
+        assertTrue("Divide by zero gives infinity as result", Double.isInfinite(calculator.divide(3.0, 0.0)));
     }
 
     @Test(priority = 4, description = "checking the division of zero by numbers")
